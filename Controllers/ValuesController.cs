119d0008e409c711200ee84eed1a8d9254ebe61e
rabin <rabin@ARHANT-RABINB>
2026-06-20 00:14:11 +0545

    added controller ans actions

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CICDPipelineTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok("HI");
+        }
+    }
+}
